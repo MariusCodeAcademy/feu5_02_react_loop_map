53faa640ab81234086a098435e8a8da1faac3555
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-21 17:06:51 +0200

    Project

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import Hero from './components/Hero';
 import Projects from './components/projects/Projects';
 import About from './components/about/About';
 
+const prArr = [
+  { id: 1, title: 'Sunflower Cottage', img: 'house5.jpg' },
+  { id: 2, title: "The Cat's Meow", img: 'cat.jpg' },
+  { id: 3, title: 'Willow House', img: 'house3.jpg' },
+  { id: 4, title: 'Tranquility Lodge', img: 'house2.jpg' },
+  { id: 'h1', title: 'Brick House', img: 'bg' },
+  { id: 'h2', title: 'Blue House', img: 'bg' },
+  { id: 'h3', title: 'Red House', img: 'bg' },
+  { id: 'h4', title: 'Straw House', img: 'bg' },
+];
+
 function App() {
   return (
     <div>
       <Header />
       <Hero />
-      <Projects list={[]} />
+      <Projects list={prArr} />
       <About />
     </div>
   );
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,44 +1,33 @@
 import Container from './../ui/Container';
 
-const prArr = [
-  { id: 1, title: '', img: '' },
-  { id: 1, title: '', img: '' },
-  { id: 1, title: '', img: '' },
-  { id: 1, title: '', img: '' },
-  { id: 'h1', title: '', img: 'bg' },
-];
+function Project({ id, title, img }) {
+  if (img === 'bg') {
+    return (
+      <div className='img-box-bg' id={id}>
+        <h3 className='img-title-bg'>{title}</h3>
+      </div>
+    );
+  }
 
-function Project({ title, img }) {
   return (
     <div className='img-box'>
       <h3 className='img-title'>{title}</h3>
-      <img src={img} alt='Some House' />
+      <img src={`/images/${img}`} alt='Some House' />
     </div>
   );
 }
 
-function Projects() {
+function Projects(props) {
   return (
     <section className='projects'>
       <Container>
         <h2 className='section-title'>Projects</h2>
         <div className='row1'>
-          <Project title='House' img='/images/house5.jpg' />
+          {props.list.map((hObj) => (
+            <Project key={hObj.id} id={hObj.id} title={hObj.title} img={hObj.img} />
+          ))}
 
-          <div className='img-box'>
-            <h3 className='img-title'>Some House</h3>
-            <img src='/images/cat.jpg' alt='Some House' />
-          </div>
-          <div className='img-box'>
-            <h3 className='img-title'>Some House</h3>
-            <img src='/images/house3.jpg' alt='Some House' />
-          </div>
-          <div className='img-box'>
-            <h3 className='img-title'>Some House</h3>
-            <img src='/images/house2.jpg' alt='Some House' />
-          </div>
-
-          <div className='img-box-bg' id='h1'>
+          {/* <div className='img-box-bg' id='h1'>
             <h3 className='img-title-bg'>Some House</h3>
           </div>
           <div className='img-box-bg' id='h2'>
@@ -49,7 +38,7 @@ function Projects() {
           </div>
           <div className='img-box-bg' id='h4'>
             <h3 className='img-title-bg'>Some Mantion</h3>
-          </div>
+          </div> */}
         </div>
       </Container>
     </section>
