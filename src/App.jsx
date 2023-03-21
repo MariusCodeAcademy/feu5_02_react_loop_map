db972c5dacd70a0b04ede22a4f8eca13ad095c06
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-21 13:05:29 +0200

    map

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const cardsArr = [
   { id: 1, title: 'Html', body: 'Body of HTml' },
   { id: 2, title: 'Css', body: 'Body of Css' },
   { id: 3, title: 'JS', body: 'Body of JS' },
+  { id: 4, title: 'Html', body: 'Body of HTml' },
+  { id: 5, title: 'Css', body: 'Body of Css' },
+  { id: 6, title: 'JS', body: 'Body of JS' },
 ];
 
 function App() {
@@ -12,9 +15,11 @@ function App() {
     <div>
       <h1>Hello</h1>
       <div className='gridThree'>
-        <Card item={cardsArr[0]} size='lg' />
-        <Card item={cardsArr[1]} />
-        <Card item={cardsArr[2]} />
+        {cardsArr.map((cardObj) => (
+          <Card key={cardObj.id} item={cardObj} />
+        ))}
+        {/* <Card item={cardsArr[1]} />
+        <Card item={cardsArr[2]} /> */}
       </div>
     </div>
   );
