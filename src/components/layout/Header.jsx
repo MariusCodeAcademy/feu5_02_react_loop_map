eb8cfa602d2c42964f10dcc1d4eeba1af6e08c10
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-21 16:04:17 +0200

    UI Container Flex

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,28 @@
+import Container from '../ui/Container';
+import Flex from '../ui/Flex';
+
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Container>
+        <Flex className='spaceBetween'>
+          <a href='/' className='logo'>
+            Logo
+          </a>
+          <nav>
+            <a href='#home' className='navLink'>
+              Home
+            </a>
+            <a href='#About' className='navLink'>
+              About
+            </a>
+            <a href='#Contact' className='navLink'>
+              Contact
+            </a>
+          </nav>
+        </Flex>
+      </Container>
+    </header>
+  );
 }
 export default Header;
---
+++ src/components/ui/Flex.jsx
@@ -0,0 +1,4 @@
+function Flex({ children, className = '' }) {
+  return <div className={`flex ${className}`}>{children}</div>;
+}
+export default Flex;
